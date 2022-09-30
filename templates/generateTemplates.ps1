0afa52c66d1e43140f0c805c053c557bfa4667a4
Paul Johnson <paul@hifi.agency>
2022-09-30 15:48:07 +0100

    Add script to generate templates

--- templates/generateTemplates.ps1
+++ templates/generateTemplates.ps1
@@ -1,359 +1,101 @@
 param(
-[string]$UmbracoVersion,
-[string]$UmbracoFormsVersion,
-[string]$YuzuDeliveryUmbracoVersion
+    [string] $UmbracoVersion = "10.2.0",
+    [string] $UmbracoFormsVersion = "10.1.2",
+    [string] $YuzuDeliveryUmbracoVersion = $null
 )
+
+if ($null -eq (Get-Command "nbgv" -ErrorAction SilentlyContinue))
+{
+    dotnet tool install -g nbgv
+}
+
+if(-Not $YuzuDeliveryUmbracoVersion)
+{
+    $YuzuDeliveryUmbracoVersion = (nbgv get-version -v NuGetPackageVersion)
+}
+
+Write-Host "###################################################################"
+Write-Host "# Umbraco Version:       $UmbracoVersion"
+Write-Host "# Forms Version:         $UmbracoFormsVersion"
+Write-Host "# Yuzu Delivery Version: $YuzuDeliveryUmbracoVersion"
+Write-Host "###################################################################"
+
+# Set current working directory to location of this file
+Push-Location -Path $PSScriptRoot
+
+# Clean up from previous runs
+Remove-Item '.tmp' -Recurse -ErrorAction Ignore
+Remove-Item 'standalone' -Recurse -ErrorAction Ignore
+Remove-Item 'testproject' -Recurse -ErrorAction Ignore
 
 Install-Module Newtonsoft.Json -Scope CurrentUser -Force
 Import-Module Newtonsoft.Json
 
-Set-Location -Path .\Templates
+# Import all the utility functions used below.
+Import-Module ./utils.psm1 -Force 
 
-Remove-Item 'Core' -Recurse -ErrorAction Ignore
-Remove-Item 'Standalone' -Recurse -ErrorAction Ignore
-Remove-Item 'Web' -Recurse -ErrorAction Ignore
+# Download Umbraco.Templates nuget package
+mkdir .tmp | Out-Null
+Invoke-WebRequest -Uri "https://www.nuget.org/api/v2/package/Umbraco.Templates/$($UmbracoVersion)" -OutFile (".tmp\umbraco.templates.$($UmbracoVersion).nupkg.zip")
+Expand-Archive -LiteralPath ".tmp\umbraco.templates.$($UmbracoVersion).nupkg.zip" -DestinationPath (".tmp\umbraco_templates")
 
-$author = "Hi-Fi Ltd"
-
-$groupIdentityStandalone    = "YuzuDelivery.Templates"
-$descriptionStandalone      = "Standalone web project for Umbraco Yuzu delivery"
-$identityStandalone         = "YuzuDelivery.Umbraco.Templates.CSharp"
-$nameStandalone             = "Yuzu Delivery"
-$shortNameStandalone        = "yuzu-delivery"
-$defaultNameStandalone      = "YuzuDelivery1"
-
-$groupIdentityCore           = "YuzuDelivery.Templates.Core"
-$descriptionCore             = "Core only project for Umbraco Yuzu delivery"
-$identityCore               = "YuzuDelivery.Umbraco.Templates.CSharp.Core"
-$nameCore                    = "Yuzu Delivery Core"
-$shortNameCore               = "yuzu-delivery-core"
-$defaultNameCore             = "YuzuDelivery.Core"
-
-$groupIdentityWeb           = "YuzuDelivery.Templates.Web"
-$descriptionWeb             = "Web only project for Umbraco Yuzu delivery"
-$identityWeb                = "YuzuDelivery.Umbraco.Templates.CSharp.Web"
-$nameWeb                    = "Yuzu Delivery Web"
-$shortNameWeb               = "yuzu-delivery-web"
-$defaultNameWeb             = "YuzuDelivery.Web1"
-
-
-#Download Umbraco.Templates nuget package
-New-Item -Path ".\" -Name "..\UmbracoTemplates" -ItemType "directory"
-Invoke-WebRequest -Uri "https://www.nuget.org/api/v2/package/Umbraco.Templates/$($UmbracoVersion)" -OutFile ("..\UmbracoTemplates\umbraco.templates.nupkg.zip")
-
-#unarchive nuget package
-New-Item -Path "..\UmbracoTemplates" -Name "unarchived" -ItemType "directory"
-Expand-Archive -LiteralPath "..\UmbracoTemplates\umbraco.templates.nupkg.zip" -DestinationPath ("..\UmbracoTemplates\unarchived")
-
-function Copy-Template {
-    param (
-        $Folder
-    )
-
-    #Copy Umbraco Project from nuget archive to specfic project
-    New-Item -Path ".\" -Name $folder -ItemType "directory"
-    Copy-Item -Path "..\UmbracoTemplates\unarchived\UmbracoProject\*" -Destination ".\$($folder)" -Recurse
+$dependencies = [ordered]@{
+    "Umbraco.Forms" = $UmbracoFormsVersion;
+    "YuzuDelivery.Umbraco.Core" = $YuzuDeliveryUmbracoVersion;
+    "YuzuDelivery.Umbraco.Forms" = $YuzuDeliveryUmbracoVersion;
+    "YuzuDelivery.Umbraco.Members" = $YuzuDeliveryUmbracoVersion;
 }
 
-function Update-ViewImports {
-    param (
-        $Folder
-    )
+###########################################################
+## Generate standalone
+###########################################################
 
-    $ViewImports = Get-Content ".\$($folder)\Views\_ViewImports.cshtml", "..\Yuzu\_ViewImports.cshtml"
-    Set-Content ".\$($folder)\Views\_ViewImports.cshtml" $ViewImports
-}
-
-function Copy-Icon {
-    param (
-        $Folder
-    )
-
-    Copy-Item -Path "..\Yuzu\Icon\icon.png" -Destination ".\$($folder)\.template.config\icon.png"
-}
-
-function Copy-Yuzu-Composer {
-    param (
-        $Folder
-    )
-
-    $pathToYuzuDir = ".\$($folder)\Yuzu"
-
-    Copy-Item -Path "..\Yuzu\Startup" -Destination $pathToYuzuDir -Recurse
-}
-
-function Copy-Yuzu-Core {
-
-    Copy-Item -Path "..\Yuzu\Core\" -Destination ".\Core" -Recurse
-}
-
-function Update-ModeslBuilder {
-    param (
-        $Folder,
-        [bool] $isWeb
-    )
-
-    $AppSettings = Get-Content ".\$($folder)\appsettings.Development.json"
-    $toFind = '"Hosting'
-
-    if($isWeb) {
-        $replaceWith = '"ModelsBuilder": {
-            "ModelsMode": "SourceCodeManual",
-            "ModelsNamespace": "YuzuDelivery.UmbracoModels",
-            "AcceptUnsafeModelsDirectory": true,
-            "ModelsDirectory": "~/../CORE_NAMESPACE/UmbracoModels"
-        },
-        "Hosting' 
-    }
-    else {
-        $replaceWith = '"ModelsBuilder": {
-            "ModelsMode": "SourceCodeManual",
-            "ModelsNamespace": "YuzuDelivery.UmbracoModels"
-        },
-        "Hosting' 
-    }
- 
-
-    $AppSettings = $AppSettings -replace $toFind, $replaceWith
-    Set-Content ".\$($folder)\appsettings.Development.json" $AppSettings
-}
-
-# Formats JSON in a nicer format than the built-in ConvertTo-Json does.
-function Format-Json([Parameter(Mandatory, ValueFromPipeline)][String] $json) {
-  $indent = 0;
-  ($json -Split '\n' |
-    % {
-      if ($_ -match '[\}\]]') {
-        # This line contains  ] or }, decrement the indentation level
-        $indent--
-      }
-      $line = (' ' * $indent * 2) + $_.TrimStart().Replace(':  ', ': ')
-      if ($_ -match '[\{\[]') {
-        # This line contains [ or {, increment the indentation level
-        $indent++
-      }
-      $line
-  }) -Join "`n"
-}
-
-function Add-Yuzu-AppSettings {
-    param (
-        $Folder,
-        [bool] $isWeb
-    )
-
-    $AppSettings = Get-Content -Path ".\$($folder)\appsettings.Development.json" -Raw
-
-    $AppSettings = $AppSettings.Trim()
-    $AppSettings = $AppSettings.Substring(0, $AppSettings.Length-3)
-
-    $yuzu = [PSCustomObject]@{
-        Yuzu = [PSCustomObject]@{
-            Core = [PSCustomObject]@{ 
-                Pages = "/yuzu/_templates/src/pages"
-                Partials = "/yuzu/_templates/src/blocks"
-                SchemaMeta = "/yuzu/_templates/paths"
-            }
-            VmGeneration = [PSCustomObject]@{ 
-                IsActive = $True 
-                AcceptUnsafeDirectory = $False 
-                Directory = "~/yuzu/viewmodels"
-            }
-            Import = [PSCustomObject]@{ 
-                IsActive = $True 
-                Config = "/yuzu/YuzuConfig.json"
-                ManualMappingDirectory = "~/yuzu/Mappings/"
-                Data = "/yuzu/_templates/data"
-                ImagesDef = "/_client/images"
-                ImagesDel = "/wwwroot/_client/images"
-            }
-        }
-    }
-
-    if($isWeb) {
-        $yuzu.Yuzu.VmGeneration.AcceptUnsafeDirectory = $True 
-        $yuzu.Yuzu.VmGeneration.Directory = "~/../CORE_NAMESPACE/ViewModels"
-        $yuzu.Yuzu.Import.ManualMappingDirectory = "~/../CORE_NAMESPACE/yuzu/Mappings/"
-    }
-
-    $yuzuString = ConvertTo-Json $yuzu | Format-Json
-    $yuzuString = $yuzuString.Substring(1, $yuzuString.Length-1) 
-
-    $output = "$($AppSettings),", $yuzuString
-
-    Set-Content ".\$($folder)\appsettings.Development.json" $output
-}
-
-function Copy-Forms-Partials {
-    param (
-        $Folder,
-        [bool] $isCore
-    )
-
-    Copy-Item -Path "..\..\Forms\Web\UI\Views\Partials\*" -Destination ".\$($folder)\Views\Partials" -Recurse
-}
-
-function Add-Project-Dependencies {
-    param (
-        $Folder,
-        [bool] $isWeb,
-        [bool] $isCore
-    )
-
-    $pathToConfig = ".\$($folder)\UmbracoProject.csproj"
-
-    $csproj = [xml](get-content $pathToConfig)
-
-    if($isCore) {
-        $newNode = [xml]"
-            <ItemGroup>
-                <PackageReference Include='Umbraco.Cms.Core' Version='$($UmbracoVersion)' />    
-                <PackageReference Include='Umbraco.Cms.Web.Common' Version='$($UmbracoVersion)' />    
-                <PackageReference Include='Umbraco.Forms.Core' Version='$($UmbracoFormsVersion)' />      
-                <PackageReference Include='YuzuDelivery.Umbraco.BlockList' Version='$($YuzuDeliveryUmbracoVersion)' />      
-                <PackageReference Include='YuzuDelivery.Umbraco.Quickstart.Core' Version='$($YuzuDeliveryUmbracoVersion)' />
-            </ItemGroup>"
-    }
-    elseif($isWeb) {
-        $newNode = [xml]"
-            <ItemGroup>
-                <PackageReference Include='Umbraco.Forms' Version='$($UmbracoFormsVersion)' />      
-                <PackageReference Include='YuzuDelivery.Umbraco.BlockList' Version='$($YuzuDeliveryUmbracoVersion)' />        
-                <PackageReference Include='YuzuDelivery.Umbraco.Quickstart.Web' Version='$($YuzuDeliveryUmbracoVersion)' />
-            </ItemGroup>"
-    }
-    else {
-        $newNode = [xml]"
-            <ItemGroup>
-                <PackageReference Include='Umbraco.Forms' Version='$($UmbracoFormsVersion)' />
-                <PackageReference Include='YuzuDelivery.Umbraco.BlockList' Version='$($YuzuDeliveryUmbracoVersion)' />  
-                <PackageReference Include='YuzuDelivery.Umbraco.Quickstart' Version='$($YuzuDeliveryUmbracoVersion)' />
-            </ItemGroup>"
-    }
-
-    $newNode = $csproj.ImportNode($newNode.ItemGroup, $true)
-    $csproj.Project.AppendChild($newNode) | out-null
-
-    $csproj.Save("$($PSScriptRoot)\Templates\$($pathToConfig)")
-}
-
-function Update-Json {
-    param (
-        $Path,
-        $Obj 
-    )
-
-    $output = [Newtonsoft.Json.JsonConvert]::SerializeObject($Obj)
-
-    Set-Content $Path $output
-}
-
-function Update-Template-Meta {
-    param (
-        $Folder,
-        [bool] $isCore,
-        [bool] $isWeb
-    )
-
-    $pathToTemplateJson = ".\$($folder)\.template.config\template.json"
-    $pathToCliJson = ".\$($folder)\.template.config\dotnetcli.host.json"
-    $pathToIdeJson = ".\$($folder)\.template.config\ide.host.json"
-
-    $json = get-content $pathToTemplateJson
-    $config = [Newtonsoft.Json.Linq.JObject]::Parse($json)
-
-    $config["author"].Value = $author
-
-    if($isCore) {
-        $config["symbols"]["version"]["defaultValue"].Value = $UmbracoVersion
-
-        $config["groupIdentity"].Value = $groupIdentityCore
-        $config["description"].Value = $descriptionCore
-        $config["identity"].Value = $identityCore
-        $config["name"].Value = $nameCore
-        $config["shortName"].Value = $shortNameCore
-        $config["shortName"].Value = $shortNameCore
-        $config["defaultName"].Value = $defaultNameCore
-    }
-    elseif($isWeb) {
-
-        $json = get-content $pathToCliJson
-        $cli = [Newtonsoft.Json.Linq.JObject]::Parse($json)
-
-        $json = get-content $pathToIdeJson
-        $ide = [Newtonsoft.Json.Linq.JObject]::Parse($json)
-
-        $config["symbols"]["CoreNamespace"] = $config["symbols"]["UmbracoVersion"]
-        $config["symbols"]["CoreNamespace"]["replaces"].Value = "CORE_NAMESPACE"
-        $config["symbols"]["CoreNamespace"]["defaultValue"].Value = "Core"
-        $config["symbols"]["CoreNamespace"]["description"].Value = "The namespace of the core application"
-
-        $cli["symbolInfo"]["CoreNamespace"] = $cli["symbolInfo"]["UseHttpsRedirect"]
-        $cli["symbolInfo"]["CoreNamespace"]["longName"].Value = "core-namespace"
-        $cli["symbolInfo"]["CoreNamespace"]["shortName"].Value = "core"
-
-        $ideElement = $ide["symbolInfo"][0]
-        $ideElement["id"].Value = "CoreNamespace"
-        #this was removed by umbraco for some reason
-        #$ideElement["name"]["text"].Value = "The namespace of the core application"
-        $ide["symbolInfo"].Add($ideElement)
-
-        $config["groupIdentity"].Value = $groupIdentityWeb
-        $config["description"].Value = $descriptionWeb
-        $config["identity"].Value = $identityWeb
-        $config["name"].Value = $nameWeb
-        $config["shortName"].Value = $shortNameWeb
-        $config["shortName"].Value = $shortNameWeb
-        $config["defaultName"].Value = $defaultNameWeb
-
-        Update-Json -Path $pathToCliJson -Obj $cli
-        Update-Json -Path $pathToIdeJson -Obj $ide
-    }
-    else {
-        $config["groupIdentity"].Value = $groupIdentityStandalone
-        $config["description"].Value = $descriptionStandalone
-        $config["identity"].Value = $identityStandalone
-        $config["name"].Value = $nameStandalone
-        $config["shortName"].Value = $shortNameStandalone
-        $config["shortName"].Value = $shortNameStandalone
-        $config["defaultName"].Value = $defaultNameStandalone
-    }
-
-    Update-Json -Path $pathToTemplateJson -Obj $config
-
-}
-
-Copy-Template -folder 'Web'
-Copy-Template -folder 'Standalone'
-Copy-Yuzu-Core
-
-Update-ViewImports -folder 'Web'
-Update-ViewImports -folder 'Standalone'
-
-Copy-Yuzu-Composer 'Core'
-Copy-Yuzu-Composer 'Standalone'
-
-Update-ModeslBuilder -folder 'Web' -isWeb $True 
-Update-ModeslBuilder -folder 'Standalone' -isWeb $False 
-
-Add-Yuzu-AppSettings -folder 'Web' -isWeb $True
+Copy-Upstream-Template -folder 'standalone'
+Update-ViewImports -folder 'standalone'
+Copy-Yuzu-Folder 'standalone'
+Update-ModeslBuilder -folder 'standalone' -isWeb $False
 Add-Yuzu-AppSettings -folder 'Standalone' -isWeb $False
+Copy-Forms-Partials -folder 'standalone'
+Copy-Icon -folder 'standalone'
+Add-Project-Dependencies-Simple -folder 'standalone' -dependencies $dependencies
+$properties = @{
+    "author" = "Hi-Fi Ltd";
+    "description" = "Standalone web project for Umbraco Yuzu delivery";
+    "identity" = "YuzuDelivery.Templates.Standalone";
+    "groupIdentity" = "YuzuDelivery.Templates.Standalone";
+    "name" = "Yuzu Delivery: Standalone";
+    "shortName" = "yuzu-standalone";
+    "defaultName" = "YuzuDelivery1";
+    "classifications" = @("Yuzu", "Umbraco", "CMS")
+}
+Update-Template-Meta-Simple 'standalone' -properties $properties
 
-Copy-Forms-Partials -folder 'Web'
-Copy-Forms-Partials -folder 'Standalone'
+Write-Host "Created standalone"
 
-Copy-Icon -folder 'Core'
-Copy-Icon -folder 'Web'
-Copy-Icon -folder 'Standalone'
+###########################################################
+## Generate testproject
+###########################################################
 
-Add-Project-Dependencies 'Core' -isCore $True
-Add-Project-Dependencies 'Web' -isCore $False
-Add-Project-Dependencies 'Standalone' -isCore $False
+Copy-Upstream-Template -folder 'testproject'
+Update-ViewImports -folder 'testproject'
+Copy-Yuzu-Folder 'testproject'
+Update-ModeslBuilder -folder 'testproject' -isWeb $False
+Add-Yuzu-AppSettings -folder 'testproject' -isWeb $False
+Copy-Forms-Partials -folder 'testproject'
+Copy-Icon -folder 'testproject'
+Add-Project-Dependencies-Simple -folder 'testproject' -dependencies $dependencies
+$properties = @{
+    "author" = "Hi-Fi Ltd";
+    "description" = "Test web project for Umbraco Yuzu delivery";
+    "identity" = "YuzuDelivery.Templates.TestProject";
+    "groupIdentity" = "YuzuDelivery.Templates.TestProject";
+    "name" = "Yuzu Delivery: Test Project";
+    "shortName" = "yuzu-test";
+    "defaultName" = "YuzuDelivery1";
+    "classifications" = @("Yuzu", "Umbraco", "CMS")
+}
+Update-Template-Meta-Simple -folder 'testproject' -properties $properties
 
-Update-Template-Meta 'Core' -isWeb $True -isCore $True
-Update-Template-Meta 'Web' -isWeb $True -isCore $False
-Update-Template-Meta 'Standalone' -isWeb $False -isCore $False
+Write-Host "Created testproject"
 
-#cleanup
-Remove-Item '..\UmbracoTemplates' -Recurse -ErrorAction Ignore
-Set-Location -Path ..
+Pop-Location
---
+++ templates/utils.psm1
@@ -0,0 +1,313 @@
+function Copy-Upstream-Template {
+    param (
+        $Folder
+    )
+
+    # Copy Umbraco Project from nuget archive to specfic project
+    New-Item -Path ".\" -Name $folder -ItemType "directory" | Out-Null
+    Copy-Item -Path ".tmp\umbraco_templates\UmbracoProject\*" -Destination ".\$($folder)" -Recurse
+}
+
+function Update-ViewImports {
+    param (
+        $Folder
+    )
+
+    $ViewImports = Get-Content ".\$($folder)\Views\_ViewImports.cshtml", ".\Yuzu\_ViewImports.cshtml"
+    Set-Content ".\$($folder)\Views\_ViewImports.cshtml" $ViewImports
+}
+
+function Copy-Icon {
+    param (
+        $Folder
+    )
+
+    Copy-Item -Path ".\Yuzu\Icon\icon.png" -Destination ".\$($folder)\.template.config\icon.png"
+}
+
+function Copy-Yuzu-Folder {
+    param (
+        $Folder
+    )
+
+    $pathToYuzuDir = ".\$($folder)\Yuzu"
+
+    Copy-Item -Path ".\Yuzu\Startup" -Destination $pathToYuzuDir -Recurse
+}
+
+function Copy-Yuzu-Core {
+    Copy-Item -Path "..\Yuzu\Core\" -Destination ".\Core" -Recurse
+}
+
+function Update-ModeslBuilder {
+    param (
+        $Folder,
+        [bool] $isWeb
+    )
+
+    $AppSettings = Get-Content ".\$($folder)\appsettings.Development.json"
+    $toFind = '"Hosting'
+
+    if($isWeb) {
+        $replaceWith = '"ModelsBuilder": {
+            "ModelsMode": "SourceCodeManual",
+            "ModelsNamespace": "YuzuDelivery.UmbracoModels",
+            "AcceptUnsafeModelsDirectory": true,
+            "ModelsDirectory": "~/../CORE_NAMESPACE/UmbracoModels"
+        },
+        "Hosting' 
+    }
+    else {
+        $replaceWith = '"ModelsBuilder": {
+            "ModelsMode": "SourceCodeManual",
+            "ModelsNamespace": "YuzuDelivery.UmbracoModels"
+        },
+        "Hosting' 
+    }
+ 
+
+    $AppSettings = $AppSettings -replace $toFind, $replaceWith
+    Set-Content ".\$($folder)\appsettings.Development.json" $AppSettings
+}
+
+# Formats JSON in a nicer format than the built-in ConvertTo-Json does.
+function Format-Json([Parameter(Mandatory, ValueFromPipeline)][String] $json) {
+  $indent = 0;
+  ($json -Split '\n' |
+    % {
+      if ($_ -match '[\}\]]') {
+        # This line contains  ] or }, decrement the indentation level
+        $indent--
+      }
+      $line = (' ' * $indent * 2) + $_.TrimStart().Replace(':  ', ': ')
+      if ($_ -match '[\{\[]') {
+        # This line contains [ or {, increment the indentation level
+        $indent++
+      }
+      $line
+  }) -Join "`n"
+}
+
+function Add-Yuzu-AppSettings {
+    param (
+        $Folder,
+        [bool] $isWeb
+    )
+
+    $AppSettings = Get-Content -Path ".\$($folder)\appsettings.Development.json" -Raw
+
+    $AppSettings = $AppSettings.Trim()
+    $AppSettings = $AppSettings.Substring(0, $AppSettings.Length-3)
+
+    $yuzu = [PSCustomObject]@{
+        Yuzu = [PSCustomObject]@{
+            Core = [PSCustomObject]@{ 
+                Pages = "/yuzu/_templates/src/pages"
+                Partials = "/yuzu/_templates/src/blocks"
+                SchemaMeta = "/yuzu/_templates/paths"
+            }
+            VmGeneration = [PSCustomObject]@{ 
+                IsActive = $True 
+                AcceptUnsafeDirectory = $False 
+                Directory = "~/yuzu/viewmodels"
+            }
+            Import = [PSCustomObject]@{ 
+                IsActive = $True 
+                Config = "/yuzu/YuzuConfig.json"
+                ManualMappingDirectory = "~/yuzu/Mappings/"
+                Data = "/yuzu/_templates/data"
+                ImagesDef = "/_client/images"
+                ImagesDel = "/wwwroot/_client/images"
+            }
+        }
+    }
+
+    if($isWeb) {
+        $yuzu.Yuzu.VmGeneration.AcceptUnsafeDirectory = $True 
+        $yuzu.Yuzu.VmGeneration.Directory = "~/../CORE_NAMESPACE/ViewModels"
+        $yuzu.Yuzu.Import.ManualMappingDirectory = "~/../CORE_NAMESPACE/yuzu/Mappings/"
+    }
+
+    $yuzuString = ConvertTo-Json $yuzu | Format-Json
+    $yuzuString = $yuzuString.Substring(1, $yuzuString.Length-1) 
+
+    $output = "$($AppSettings),", $yuzuString
+
+    Set-Content ".\$($folder)\appsettings.Development.json" $output
+}
+
+# TODO: No longer required after we have RCL's
+function Copy-Forms-Partials {
+    param (
+        $Folder,
+        [bool] $isCore
+    )
+
+    Copy-Item -Path "..\src\Forms\Web\UI\Views\Partials\*" -Destination ".\$($folder)\Views\Partials" -Recurse
+}
+
+function Add-Project-Dependencies {
+    param (
+        $Folder,
+        [bool] $isWeb,
+        [bool] $isCore
+    )
+
+    $pathToConfig = (Get-Item ".\$($folder)\UmbracoProject.csproj")
+
+    $csproj = [xml](get-content $pathToConfig)
+
+    if($isCore) {
+        $newNode = [xml]"
+            <ItemGroup>
+                <PackageReference Include='Umbraco.Cms.Core' Version='$($UmbracoVersion)' />    
+                <PackageReference Include='Umbraco.Cms.Web.Common' Version='$($UmbracoVersion)' />    
+                <PackageReference Include='Umbraco.Forms.Core' Version='$($UmbracoFormsVersion)' />      
+                <PackageReference Include='YuzuDelivery.Umbraco.BlockList' Version='$($YuzuDeliveryUmbracoVersion)' />      
+                <PackageReference Include='YuzuDelivery.Umbraco.Quickstart.Core' Version='$($YuzuDeliveryUmbracoVersion)' />
+            </ItemGroup>"
+    }
+    elseif($isWeb) {
+        $newNode = [xml]"
+            <ItemGroup>
+                <PackageReference Include='Umbraco.Forms' Version='$($UmbracoFormsVersion)' />      
+                <PackageReference Include='YuzuDelivery.Umbraco.BlockList' Version='$($YuzuDeliveryUmbracoVersion)' />        
+                <PackageReference Include='YuzuDelivery.Umbraco.Quickstart.Web' Version='$($YuzuDeliveryUmbracoVersion)' />
+            </ItemGroup>"
+    }
+    else {
+        $newNode = [xml]"
+            <ItemGroup>
+                <PackageReference Include='Umbraco.Forms' Version='$($UmbracoFormsVersion)' />
+                <PackageReference Include='YuzuDelivery.Umbraco.BlockList' Version='$($YuzuDeliveryUmbracoVersion)' />  
+                <PackageReference Include='YuzuDelivery.Umbraco.Quickstart' Version='$($YuzuDeliveryUmbracoVersion)' />
+            </ItemGroup>"
+    }
+
+    $newNode = $csproj.ImportNode($newNode.ItemGroup, $true)
+    $csproj.Project.AppendChild($newNode) | out-null
+
+    $csproj.Save($pathToConfig.FullName)
+}
+
+function Add-Project-Dependencies-Simple {
+    param (
+        $Folder,
+        [System.Collections.Specialized.OrderedDictionary] $dependencies
+    )
+
+    $pathToConfig = (Get-Item ".\$($folder)\UmbracoProject.csproj")
+
+    $csproj = [xml](get-content $pathToConfig)
+
+    foreach ($key in $dependencies.Keys) {
+        $node = $csproj.CreateElement("PackageReference")
+        $node.SetAttribute("Include", $key)
+        $node.SetAttribute("Version", $dependencies[$key])
+        $csproj.Project.ItemGroup[0].AppendChild($node) | Out-Null
+    }
+
+    $csproj.Save($pathToConfig.FullName)
+}
+
+function Update-Json {
+    param (
+        $Path,
+        $Obj 
+    )
+
+    $output = [Newtonsoft.Json.JsonConvert]::SerializeObject($Obj)
+
+    Set-Content $Path $output
+}
+
+function Update-Template-Meta {
+    param (
+        $Folder,
+        [bool] $isCore,
+        [bool] $isWeb
+    )
+
+    $pathToTemplateJson = ".\$($folder)\.template.config\template.json"
+    $pathToCliJson = ".\$($folder)\.template.config\dotnetcli.host.json"
+    $pathToIdeJson = ".\$($folder)\.template.config\ide.host.json"
+
+    $json = get-content $pathToTemplateJson
+    $config = [Newtonsoft.Json.Linq.JObject]::Parse($json)
+
+    $config["author"].Value = $author
+
+    if($isCore) {
+        $config["symbols"]["version"]["defaultValue"].Value = $UmbracoVersion
+
+        $config["groupIdentity"].Value = $groupIdentityCore
+        $config["description"].Value = $descriptionCore
+        $config["identity"].Value = $identityCore
+        $config["name"].Value = $nameCore
+        $config["shortName"].Value = $shortNameCore
+        $config["shortName"].Value = $shortNameCore
+        $config["defaultName"].Value = $defaultNameCore
+    }
+    elseif($isWeb) {
+
+        $json = get-content $pathToCliJson
+        $cli = [Newtonsoft.Json.Linq.JObject]::Parse($json)
+
+        $json = get-content $pathToIdeJson
+        $ide = [Newtonsoft.Json.Linq.JObject]::Parse($json)
+
+        $config["symbols"]["CoreNamespace"] = $config["symbols"]["UmbracoVersion"]
+        $config["symbols"]["CoreNamespace"]["replaces"].Value = "CORE_NAMESPACE"
+        $config["symbols"]["CoreNamespace"]["defaultValue"].Value = "Core"
+        $config["symbols"]["CoreNamespace"]["description"].Value = "The namespace of the core application"
+
+        $cli["symbolInfo"]["CoreNamespace"] = $cli["symbolInfo"]["UseHttpsRedirect"]
+        $cli["symbolInfo"]["CoreNamespace"]["longName"].Value = "core-namespace"
+        $cli["symbolInfo"]["CoreNamespace"]["shortName"].Value = "core"
+
+        $ideElement = $ide["symbolInfo"][0]
+        $ideElement["id"].Value = "CoreNamespace"
+        #this was removed by umbraco for some reason
+        #$ideElement["name"]["text"].Value = "The namespace of the core application"
+        $ide["symbolInfo"].Add($ideElement)
+
+        $config["groupIdentity"].Value = $groupIdentityWeb
+        $config["description"].Value = $descriptionWeb
+        $config["identity"].Value = $identityWeb
+        $config["name"].Value = $nameWeb
+        $config["shortName"].Value = $shortNameWeb
+        $config["shortName"].Value = $shortNameWeb
+        $config["defaultName"].Value = $defaultNameWeb
+
+        Update-Json -Path $pathToCliJson -Obj $cli
+        Update-Json -Path $pathToIdeJson -Obj $ide
+    }
+    else {
+        $config["groupIdentity"].Value = $groupIdentityStandalone
+        $config["description"].Value = $descriptionStandalone
+        $config["identity"].Value = $identityStandalone
+        $config["name"].Value = $nameStandalone
+        $config["shortName"].Value = $shortNameStandalone
+        $config["shortName"].Value = $shortNameStandalone
+        $config["defaultName"].Value = $defaultNameStandalone
+    }
+
+    Update-Json -Path $pathToTemplateJson -Obj $config
+}
+
+function Update-Template-Meta-Simple {
+    param (
+        $Folder,
+        [System.Collections.Hashtable] $properties
+    )
+
+    $pathToTemplateJson = ".\$($folder)\.template.config\template.json"
+    $json = get-content $pathToTemplateJson
+    $config = [Newtonsoft.Json.Linq.JObject]::Parse($json)
+
+    foreach ($key in $properties.Keys) {
+        $config[$key] = (ConvertTo-Json $properties[$key])
+    }
+
+    Update-Json -Path $pathToTemplateJson -Obj $config
+}

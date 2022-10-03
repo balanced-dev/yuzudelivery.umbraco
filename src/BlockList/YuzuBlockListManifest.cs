a3e00f16a166ef1dc9467d64bdf2b0de97e4aafb
Phil Whittaker <phil@hifi.agency>
2022-10-03 15:36:06 +0100

    tru to get rcl working

---
+++ src/BlockList/YuzuBlockListManifest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using Umbraco.Cms.Core.Manifest;
+
+namespace YuzuDelivery.Umbraco.BlockList
+{
+
+    public class DocTypeGridEditorManifestFilter : IManifestFilter
+    {
+        public void Filter(List<PackageManifest> manifests)
+        {
+            manifests.Add(new PackageManifest()
+            {
+                Scripts = new[]
+                {
+                    "/App_Plugins/YuzuBlockList/GridContentColumnsSettingsController.js",
+                    "/App_Plugins/YuzuBlockList/GridContentItem.js",
+                    "/App_Plugins/YuzuBlockList/GridContentSection.js"
+                },
+                Stylesheets = new[]
+                {
+                    "/App_Plugins/YuzuBlockList/GridSection.css"
+                }
+
+            });
+        }
+    }
+
+}

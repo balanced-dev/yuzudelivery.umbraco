4517dea48a85873ac00791ca9cfb4eac71caa77e
Paul Johnson <paul@hifi.agency>
2022-10-13 12:09:59 +0100

    Fix - Restore ability to use absolute paths for view models directory

--- src/Core/Startup/DefaultUmbracoVmBuilderConfig.cs
+++ src/Core/Startup/DefaultUmbracoVmBuilderConfig.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
-using YuzuDelivery.Core.Settings;
 using YuzuDelivery.Core.ViewModelBuilder;
 using YuzuDelivery.Umbraco.Core.Settings;
 using ConfigurationException = Umbraco.Cms.Core.Exceptions.ConfigurationException;
@@ -16,13 +14,11 @@ namespace YuzuDelivery.Umbraco.Core.Startup
         public DefaultUmbracoVmBuilderConfig(
             IEnumerable<IUpdateableVmBuilderConfig> vmBuilderConfigs,
             IHostEnvironment hostEnvironment,
-            IOptionsMonitor<CoreSettings> coreSettings,
             IOptionsMonitor<VmGenerationSettings> vmGenerationSettings)
             : base(vmBuilderConfigs)
         {
             EnableViewmodelsBuilder = vmGenerationSettings.CurrentValue.IsActive;
-            GeneratedViewmodelsOutputFolder = GetModelsDirectory(hostEnvironment.ContentRootPath,
-                vmGenerationSettings.CurrentValue.Directory, vmGenerationSettings.CurrentValue.AcceptUnsafeDirectory);
+            GeneratedViewmodelsOutputFolder = GetModelsDirectory(hostEnvironment.ContentRootPath, vmGenerationSettings.CurrentValue.Directory, vmGenerationSettings.CurrentValue.AcceptUnsafeDirectory);
         }
 
         private string GetModelsDirectory(string root, string config, bool acceptUnsafe)
@@ -30,23 +26,37 @@ namespace YuzuDelivery.Umbraco.Core.Startup
             // making sure it is safe, ie under the website root,
             // unless AcceptUnsafeModelsDirectory and then everything is OK.
 
-            if (!Path.IsPathRooted(root))
+            if (!Path.IsPathFullyQualified(root))
             {
                 ThrowException($"Root is not rooted \"{root}\".");
             }
 
-            var dir = Path.Combine(root, StringExtensions.TrimStart(config, "~/"));
-
-            // sanitize - GetFullPath will take care of any relative
-            // segments in path, eg '../../foo.tmp' - it may throw a SecurityException
-            // if the combined path reaches illegal parts of the filesystem
-            dir = Path.GetFullPath(dir);
             root = Path.GetFullPath(root);
 
-            if (!dir.StartsWith(root) && !acceptUnsafe)
-                ThrowException($"Invalid models directory \"{config}\".");
+            if (Path.IsPathFullyQualified(config))
+            {
 
-            return dir;
+               var dir = Path.GetFullPath(config);
+
+               if (!dir.StartsWith(root) && !acceptUnsafe)
+                   ThrowException($"Invalid models directory \"{config}\".");
+
+               return dir;
+            }
+            else
+            {
+                var dir = Path.Combine(root, StringExtensions.TrimStart(config, "~/"));
+
+                // sanitize - GetFullPath will take care of any relative
+                // segments in path, eg '../../foo.tmp' - it may throw a SecurityException
+                // if the combined path reaches illegal parts of the filesystem
+                dir = Path.GetFullPath(dir);
+
+                if (!dir.StartsWith(root) && !acceptUnsafe)
+                    ThrowException($"Invalid models directory \"{config}\".");
+
+                return dir;
+            }
         }
 
         private void ThrowException(string message)

15f43cd3d7b43fe8ad0e225e3cbabee05764c6ae
Paul Johnson <paul@hifi.agency>
2022-10-21 14:55:10 +0100

    allow local retries

--- acceptance/playwright.config.ts
+++ acceptance/playwright.config.ts
@@ -29,8 +29,8 @@ const config: PlaywrightTestConfig = {
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  /* always retry at least once */
+  retries: 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */

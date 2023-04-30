28318e8db758d5c216277cb2145bea085f969458
philwhittaker <phil@hifi.agency>
2023-04-30 14:26:06 +0100

    middleware

--- src/Core/Middleware/YuzuLoadedSchemaMiddleware.cs
+++ src/Core/Middleware/YuzuLoadedSchemaMiddleware.cs
@@ -78,14 +78,16 @@ public class YuzuLoadedSchemaMiddleware : IMiddleware
             }
             catch (Exception ex)
             {
-                Exception = ex;
+                Exception = ex.Message;
+                ExceptionStackTrace = ex.StackTrace;
             }
 
         }
 
         public List<string> FileProviders { get; set; }
         public IEnumerable<IFileInfo> Files { get; set; }
-        public Exception? Exception { get; set; }
+        public string? Exception { get; set; }
+        public string? ExceptionStackTrace { get; set; }
 
         private void AddFileProviderToReport(IFileProvider f, FileProviderMiddleWareReport report)
         {
